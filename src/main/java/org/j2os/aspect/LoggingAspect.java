bc254f9cf8f02310f37ecbb18d31c045e4c099ca
Monireh Jamshidi <monireh.jamshidi@gmail.com>
2025-12-09 13:06:07 +0330

    Im LoggingAspect wurde SLF4J Logging verwendet

--- src/main/java/org/j2os/aspect/LoggingAspect.java
+++ src/main/java/org/j2os/aspect/LoggingAspect.java
@@ -5,21 +5,25 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 
 @Aspect
 @Component
+
 public class LoggingAspect {
+    private static final Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
 
     @Before("execution(* org.j2os..*(..))")
     public void logBefore(JoinPoint joinPoint){
-        System.out.println("➡️ Before: " + joinPoint.getSignature()
+        logger.info("➡️ Before: " + joinPoint.getSignature()
                 + " args: " + Arrays.toString(joinPoint.getArgs()));
     }
 
-    @Around("execution(* org.j2os.service.. *(..))")
+    @Around("execution(* org.j2os.service..*(..))")
     public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable{
 
         long start = System.currentTimeMillis();
@@ -27,7 +31,7 @@ public class LoggingAspect {
             return joinPoint.proceed();
         } finally {
             long duration = System.currentTimeMillis() - start;
-            System.out.println("⏱️ Execution Time: " + joinPoint.getSignature() + " duration: " + duration + " ms");
+            logger.info("⏱️ Execution Time: " + joinPoint.getSignature() + " duration: " + duration + " ms");
         }
     }
 }
